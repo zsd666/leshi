730edd533ef91cb2809eb104278306d7cac60eb0
zsd <1196023965@qq.com>
2020-03-03 21:53:40 +0800

    首页完成登录注册布局样式完成

--- src/js/login.js
+++ src/js/login.js
@@ -1,34 +1,30 @@
-[
-    {
-      title: "爆款推荐"
-      id: 1,
-      list: [ 
-        {
-          name: 'Y332',
-          id: 101,
-          url:"../imga/"
-        },
-        {
-          name: 'Y443',
-          id: 102,
-          url:"../"
-        }
-      ]
-    },
-    {
-      title: "屏幕尺寸",
-      id: 2,
-      list: [
-        {
-          name: 'X332',
-          id: 201,
-          url:""
-        },
-        {
-          name: 'XZ44',
-          id: 202,
-          url:""
-        }
-      ]
-    }
-  ]
+const cards = document.querySelectorAll('.card');
+
+/* View Controller
+                                                  -----------------------------------------*/
+const btns = document.querySelectorAll('.js-btn');
+btns.forEach(btn => {
+  btn.addEventListener('click', on_btn_click, true);
+  btn.addEventListener('touch', on_btn_click, true);
+});
+
+function on_btn_click(e) {
+  const nextID = e.currentTarget.getAttribute('data-target');
+  const next = document.getElementById(nextID);
+  if (!next) return;
+  bg_change(nextID);
+  view_change(next);
+  return false;
+}
+
+/* Add class to the body */
+function bg_change(next) {
+  document.body.className = '';
+  document.body.classList.add('is-' + next);
+}
+
+/* Add class to a card */
+function view_change(next) {
+  cards.forEach(card => {card.classList.remove('is-show');});
+  next.classList.add('is-show');
+}
